b6e6c13c30d1c94c6ac3b41a70fb06fa1c499d42
noureen <noureenshefeedh@gmail.com>
2026-08-22 18:40:45 +0530

    render fux

--- server/src/index.js
+++ server/src/index.js
@@ -33,6 +33,7 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
